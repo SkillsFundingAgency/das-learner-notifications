82cbf6ffddec626f0758c1743d7f4fc14d1dba2e
abroodie <abroodie@outlook.com>
2026-06-02 09:52:53 +0100

    Tidied up database project

--- src/SFA.DAS.LearnerNotifications.Database/Tables/Notification.sql
+++ src/SFA.DAS.LearnerNotifications.Database/Tables/Notification.sql
@@ -1,20 +1,19 @@
 ﻿CREATE TABLE [dbo].[Notification](
-    [NotificationId] [bigint] IDENTITY(1,1) NOT NULL,
-    [CorrelationId] [uniqueidentifier] NULL,
-    [LearnerAccountId] [uniqueidentifier] NULL,
+    [Id] [bigint] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_Notification] PRIMARY KEY CLUSTERED,
+    [CorrelationId] [uniqueidentifier] Not Null,
+    [LearnerAccountId] [uniqueidentifier] Not Null,
     [Category] [nvarchar](255) NULL,
-    [Heading] [nvarchar](255) NULL,
-    [Body] [nvarchar](max) NULL,
-    [StatusId] [tinyint] NULL,
-    [NotificationTime] [datetime] NULL,
-    [TimeToExpire] [datetime] NULL,
-    [TimeReceived] [datetime] NULL,
-    [Link] [nvarchar](500) NULL
+    [Heading] [nvarchar](255) NOT NULL,
+    [Body] [nvarchar](max) NOT NULL,
+    LinkUrl [nvarchar](max) NULL,
+    [StatusId] [tinyint] NOT NULL Constraint [FK_Notification_NotificationStatus] Foreign Key References [dbo].[NotificationStatus]([StatusId]),
+    [NotificationTime] [datetime2] NULL,
+    [TimeToExpire] [datetime2] NULL,
+    [TimeReceived] [datetime2] NULL,
 ) ON [PRIMARY]
 GO
 
-ALTER TABLE [dbo].[Notification] ADD PRIMARY KEY CLUSTERED 
-(
-    [NotificationId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+CREATE INDEX [IX_Notification_CorrelationId] ON [dbo].[Notification] (
+    CorrelationId
+)
 GO
---
+++ src/SFA.DAS.LearnerNotifications.Database/Tables/NotificationStatus.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[NotificationStatus](
+    [StatusId] [tinyint] NOT NULL CONSTRAINT [PK_NotificationStatus] PRIMARY KEY CLUSTERED,
+    [Description] [nvarchar](50) NOT NULL
+) ON [PRIMARY]
+GO
