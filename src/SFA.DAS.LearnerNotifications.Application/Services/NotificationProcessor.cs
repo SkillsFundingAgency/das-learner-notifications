ebdba15a51711d52b0058dc0ffbcee86d40f2cf8
abroodie <abroodie@outlook.com>
2026-06-08 23:55:06 +0100

    processor test now passes

--- src/SFA.DAS.LearnerNotifications.Application/Services/NotificationProcessor.cs
+++ src/SFA.DAS.LearnerNotifications.Application/Services/NotificationProcessor.cs
@@ -1,6 +1,8 @@
-﻿using SFA.DAS.LearnerNotifications.Messages.Commands;
+﻿using SFA.DAS.LearnerNotifications.Application.Data;
+using SFA.DAS.LearnerNotifications.Messages.Commands;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace SFA.DAS.LearnerNotifications.Application.Services
@@ -12,9 +14,40 @@ namespace SFA.DAS.LearnerNotifications.Application.Services
 
     public class NotificationProcessor : INotificationProcessor
     {
-        public Task Process(SendNotification notification)
+        private readonly ILearnerNotificationsDataContext dataContext;
+
+        public NotificationProcessor(ILearnerNotificationsDataContext dataContext)
         {
-            throw new NotImplementedException();
+            this.dataContext = dataContext ?? throw new ArgumentNullException(nameof(dataContext));
+        }
+
+        public async Task Process(SendNotification notification)
+        {
+            var model = new Models.Notification
+            {
+                CorrelationId = notification.CorrelationId,
+                LearnerAccountId = notification.LearnerAccountId,
+                Category = notification.Category,
+                Heading = notification.Heading,
+                Body = notification.Body,
+                LinkUrl = notification.LinkUrl,
+                NotificationTime = notification.NotificationTime,
+                TimeToExpire = notification.TimeToExpire,
+                Urgency = Convert(notification.Urgency)
+            };
+
+            await dataContext.SaveNotification(model);
+        }
+
+        private Models.NotificationUrgency Convert(Urgency urgency)
+        {
+            return urgency switch
+            {
+                Urgency.Low => Models.NotificationUrgency.Low,
+                Urgency.Medium => Models.NotificationUrgency.Medium,
+                Urgency.High => Models.NotificationUrgency.High,
+                _ => throw new ArgumentOutOfRangeException(nameof(urgency), $"Unexpected urgency value: {urgency}")
+            };
         }
     }
 }
